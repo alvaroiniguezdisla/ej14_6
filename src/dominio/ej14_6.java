02cf0c43ac4fbf987e3fac3c90a071ec4722c305
Álvaro <ainiguezdislaa@gmail.com>
2023-02-26 18:06:15 +0100

    ej terminado

--- src/dominio/ej14_6.java
+++ src/dominio/ej14_6.java
@@ -1,2 +1,17 @@
-package dominio;public class ej14_6 {
+package dominio;
+import java.util.stream.Stream;
+
+public class ej14_6 {
+    public static int media(int[] lista){
+        return Stream.iterate(0,i->i+1).limit(lista.length).map(i->lista[i]).reduce(0,(a,b)->a+b)/lista.length;
+    }
+
+    public static int cuadrados(int[] lista){
+        return Stream.iterate(0,i->i+1).limit(lista.length).map(i->lista[i]).map(i->i*i).reduce(0,(a,b)->a+b);
+    }
+    public static double desviacion(int[] lista){
+        double media=media(lista);
+        double cuadrados=cuadrados(lista);
+        return Math.sqrt((cuadrados/lista.length)-(media*media));
+    }
 }
